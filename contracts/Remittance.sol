14de8df9c4c5eaf8640561aea6d3732bd21ec4f0
micah <micah@atonomi.io>
2018-08-02 23:44:59 -0700

    Removes vendor password since hashing with their address is sufficient

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -8,12 +8,12 @@ contract Remittance {
 
     event WithdrawalMade(address _by, uint _amount);
 
-    function generateHash(address vendor, string recipientPassword, string vendorPassword) private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(vendor, recipientPassword, vendorPassword));
+    function generateHash(address vendor, string recipientPassword) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(vendor, recipientPassword));
     }
 
     /// @notice funds the contract with ETH
-    /// @param pwHash would have been created based on the vendor's address and 2 passwords
+    /// @param pwHash would have been created based on the recipient's password and the vendor's address
     function sendMoney(bytes32 pwHash) public payable {
         require(msg.value > 0, "msg.value equals 0");
         require(remittances[pwHash] == 0, "an unclaimed remittance already exists for this account");
@@ -22,8 +22,8 @@ contract Remittance {
     }
 
     /// @notice withdraw ETH funds from contract
-    function withdraw(string password1, string password2) public {
-        bytes32 pwHash = generateHash(msg.sender, password1, password2);
+    function withdraw(string password) public {
+        bytes32 pwHash = generateHash(msg.sender, password);
         uint allowance = remittances[pwHash];
         require(allowance > 0, "nothing to withdraw, allowance equals 0");
         remittances[pwHash] = 0;
